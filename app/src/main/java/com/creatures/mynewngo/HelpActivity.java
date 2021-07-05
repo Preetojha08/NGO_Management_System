9833d5973ecaeaecb0d1a309137461696af16cae
Preet Ojha <ozapreet08@gmail.com>
2021-07-05 20:02:02 +0530

    Navigation Drawer commit and Help (Done RecyclerView)

--- app/src/main/java/com/creatures/mynewngo/HelpActivity.java
+++ app/src/main/java/com/creatures/mynewngo/HelpActivity.java
@@ -2,10 +2,23 @@ package com.creatures.mynewngo;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class HelpActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    List<String> titles;
+    List<String> sub_titles;
+    List<Integer> images;
+
+    HelperCardRecyclerAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,5 +27,41 @@ public class HelpActivity extends AppCompatActivity {
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar_help);
         setSupportActionBar(toolbar);
+
+        titles = new ArrayList<>();
+        sub_titles = new ArrayList<>();
+        images = new ArrayList<>();
+
+        recyclerView=(RecyclerView)findViewById(R.id.help_cards_recycler_view);
+
+        titles.add("First Item");
+        titles.add("Second Item");
+        titles.add("Third Item");
+        titles.add("Fourth Item");
+        titles.add("Fifth Item");
+        titles.add("Sixth Item");
+
+        sub_titles.add("Item 1");
+        sub_titles.add("Item 2");
+        sub_titles.add("Item 3");
+        sub_titles.add("Item 4");
+        sub_titles.add("Item 5");
+        sub_titles.add("Item 6");
+
+        images.add(R.drawable.info);
+        images.add(R.drawable.info);
+        images.add(R.drawable.info);
+        images.add(R.drawable.info);
+        images.add(R.drawable.info);
+        images.add(R.drawable.info);
+
+        adapter = new HelperCardRecyclerAdapter(this,titles,sub_titles,images);
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(this,2,GridLayoutManager.VERTICAL,false);
+        recyclerView.setLayoutManager(gridLayoutManager);
+
+        //recyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+        recyclerView.setAdapter(adapter);
+
+
     }
 }
---
+++ app/src/main/java/com/creatures/mynewngo/HelperCardRecyclerAdapter.java
@@ -0,0 +1,63 @@
+package com.creatures.mynewngo;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+import java.util.zip.Inflater;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class HelperCardRecyclerAdapter extends RecyclerView.Adapter<HelperCardRecyclerAdapter.card_view_holder>{
+
+    List<String> titles;
+    List<String> sub_titles;
+    List<Integer> images;
+    LayoutInflater layoutInflater;
+
+    public HelperCardRecyclerAdapter(Context context, List<String> titles, List<String> sub_titles, List<Integer> images) {
+        this.titles = titles;
+        this.sub_titles = sub_titles;
+        this.images = images;
+        this.layoutInflater = LayoutInflater.from(context);
+    }
+
+    @NonNull
+    @Override
+    public card_view_holder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        View view = layoutInflater.inflate(R.layout.help_cards,parent,false);
+        return new card_view_holder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull HelperCardRecyclerAdapter.card_view_holder holder, int position) {
+        holder.tv_help_card_heading.setText(titles.get(position));
+        holder.tv_help_card_subheading.setText(sub_titles.get(position));
+        holder.tv_help_card_image_view.setImageResource(images.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return titles.size();
+    }
+
+    public class card_view_holder extends RecyclerView.ViewHolder
+    {
+        TextView tv_help_card_heading,tv_help_card_subheading;
+        ImageView tv_help_card_image_view;
+        public card_view_holder(@NonNull View itemView) {
+            super(itemView);
+
+            tv_help_card_heading=(TextView)itemView.findViewById(R.id.help_card_text_view_1);
+            tv_help_card_subheading=(TextView)itemView.findViewById(R.id.help_card_text_view_2);
+            tv_help_card_image_view=(ImageView)itemView.findViewById(R.id.help_card_image_view);
+        }
+    }
+    
+}
