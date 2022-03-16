414952816dd1ce5bec16ee78a28515427a1d06ff
Preet Ojha <ozapreet08@gmail.com>
2022-03-17 01:13:05 +0530

    New Commit

--- app/src/main/java/com/creatures/mynewngo/MainActivity.java
+++ app/src/main/java/com/creatures/mynewngo/MainActivity.java
@@ -30,7 +30,7 @@ public class MainActivity extends AppCompatActivity {
             public void run() {
 
 
-                Intent i = new Intent(MainActivity.this,HomeActivity.class);
+                Intent i = new Intent(MainActivity.this,RegistrationActivity.class);
                 startActivity(i);
                 finish();
             }
