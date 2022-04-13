dbcca77a6e327d101afdc19e28a08a9c1aa9520a
Preet Ojha <ozapreet08@gmail.com>
2022-04-13 23:49:11 +0530

    Updated Gradle File

---
+++ app/src/main/java/com/creatures/mynewngo/SingleActivityDetails.java
@@ -0,0 +1,14 @@
+    package com.creatures.mynewngo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SingleActivityDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_single_details);
+    }
+}
